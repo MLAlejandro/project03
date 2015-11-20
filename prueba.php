6749b182228a55dd356de389bb42f35a4f4aa54a
MLAlejandro <96034.joan23@fje.edu>
2015-11-20 01:22:28 +0100

    Eliminacion archibo prueba.php
    no sirve de nada, simplemente se ha colado

--- prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST["escuchar"]))
-{
-	echo "hola";
-}
-?>
-<form method="post" action="<?=$_SERVER['PHP_SELF']?>?jnaifjnsafja">
-<input type="submit" name="escuchar" value="Escuchar">
-</form>
